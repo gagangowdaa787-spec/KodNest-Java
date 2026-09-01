e7bb0273e3ef86173d105efb8bace4e6d1eed0be
gagangowdaa787-spec <gagangowdaa787@gmail.com>
2026-09-01 12:59:42 +0530

    message

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+       System.out.println("welcome to Java");
+       System.out.println("I am in Java Track");
     }
 }
